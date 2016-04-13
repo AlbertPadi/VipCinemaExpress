bceb29ebda3efd4d45628bbab0e0528658306a59
AlbertPadi <jhoanpadi@gmail.com>
2016-04-13 09:51:41 -0400

    cReservas

---
+++ VIPCinemaExpress/Rpts/CinesReporte.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+namespace VIPCinemaExpress.Rpts
+{
+    public partial class CinesReporte : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!IsPostBack)
+            {
+                Cines cine = new Cines();
+
+                //CinesReportViewer.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Local;
+                //CinesReportViewer.Reset();
+                //CinesReportViewer.LocalReport.ReportPath = Server.MapPath(@"~\Reportes/CinesRpt.rdlc");
+
+                //CinesReportViewer.LocalReport.DataSources.Clear();
+
+                //CinesReportViewer.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSet1", cine.Listado(" * ", "1=1", "")));
+                //CinesReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
--- VIPCinemaExpress/adm/Consultas/cReservaciones.aspx.cs
+++ VIPCinemaExpress/adm/Consultas/cReservaciones.aspx.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using BLL;
 namespace VIPCinemaExpress.adm.Consultas
 {
     public partial class cReservaciones : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Reservaciones reservas = new Reservaciones();
+            DataTable dt = new DataTable();
+            string filtro = "1=1";
 
+            if (DatosDropDownList.SelectedIndex == 0)
+            {
+                if (DatosTextBox.Text.Trim().Length == 0)
+                {
+                    filtro = "1=1";
+                }
+                else
+                {
+                    filtro = "ReservacionId = " + DatosTextBox.Text;
+                }
+            }
+
+            else if (DatosDropDownList.SelectedIndex == 1)
+                if (DatosTextBox.Text.Trim().Length == 0)
+                {
+                    filtro = "1=1";
+                }
+                else
+                {
+
+                    filtro = "UsuarioId like '%" + DatosTextBox.Text + "%'";
+                }
+            
+
+            dt = reservas.Listado(" * ", filtro, "ReservacionId ASC");
+            DatosGridView.DataSource = dt;
+            DatosGridView.DataBind();
         }
     }
 }
